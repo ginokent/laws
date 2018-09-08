24812db7e0bc8cd588824cdeba4f84539c338295
newtstat <29125616+newtstat@users.noreply.github.com>
2018-09-09 04:35:50 +0900

    add log for test

--- .tools/test.sh
+++ .tools/test.sh
@@ -2,26 +2,32 @@
 
 set -x
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  List buckets"
 TEST_01="
 $(./lw s3 list | head -10)
 "
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  List objects"
 TEST_02="
 $(./lw s3 list "s3://$(set +x; echo "${TEST_01}" | grep -Ev '^$|\$' | head -1 | sed 's/[^[:space:]]*[[:space:]]//')" | head -10)
 "
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  Get object"
 TEST_03="
 $(./lw s3 get "s3://$(set +x; echo "${TEST_01}" | grep -Ev '^$|\$' | head -1 | sed 's/[^[:space:]]*[[:space:]]//')/$(set +x; echo "${TEST_02}" | grep -Ev '^$|\$' | head -1 | sed 's/[^[:space:]]*[[:space:]]//')" | head -10 | strings)
 "
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  List buckets"
 TEST_04="
 $(./lw s3 list | tail -10)
 "
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  List objects"
 TEST_05="
 $(./lw s3 list "s3://$(set +x; echo "${TEST_04}" | grep -Ev '^$|\$' | tail -1 | sed 's/[^[:space:]]*[[:space:]]//')" | tail -10)
 "
 
+printf "\n\e[32m%s\e[0m\n" "$(LANG=C date) [TEST]  Get object"
 TEST_06="
 $(./lw s3 get "s3://$(set +x; echo "${TEST_04}" | grep -Ev '^$|\$' | tail -1 | sed 's/[^[:space:]]*[[:space:]]//')/$(set +x; echo "${TEST_05}" | grep -Ev '^$|\$' | tail -1 | sed 's/[^[:space:]]*[[:space:]]//')" | tail -10 | strings)
 "
