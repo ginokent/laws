411f77355ce29d7a416af614d8897f0803036fbd
newtstat <29125616+newtstat@users.noreply.github.com>
2018-09-19 02:52:32 +0900

    add comments

--- .tools/test-ec2.sh
+++ .tools/test-ec2.sh
@@ -10,7 +10,7 @@ echo "LAWS_LOCAL_REPOSITORY  : ${LAWS_LOCAL_REPOSITORY:="${HOME}/djeeno/laws/"}"
 echo "LAWS_REMOTE_HOST       : ${LAWS_REMOTE_HOST:="52.193.197.82"}"
 echo "LAWS_REMOTE_USER       : ${LAWS_REMOTE_USER:="ubuntu"}"
 echo "LAWS_REMOTE_REPOSITORY : ${LAWS_REMOTE_REPOSITORY:="/home/${LAWS_REMOTE_USER}/laws/"}"
-echo "LAWS_REMOTE_COMMAND    : ${LAWS_REMOTE_COMMAND:="cd ${LAWS_REMOTE_REPOSITORY} && make"}"
+echo "LAWS_REMOTE_COMMAND    : ${LAWS_REMOTE_COMMAND:="cd ${LAWS_REMOTE_REPOSITORY} && make test"}"
 
 # enable print commands
 set -x
