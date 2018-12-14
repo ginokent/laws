7f7160c967eb2677b98ddfa266ed03cea7b35629
newtstat <29125616+newtstat@users.noreply.github.com>
2018-12-14 15:41:14 +0900

    laws: fix completion

--- .tools/test.sh
+++ .tools/test.sh
@@ -5,7 +5,7 @@ DIRECTORY="$(cd -- "$(dirname -- "$0")" && pwd -P)"
 PROGRAM="$(dirname -- "${DIRECTORY}")/laws"
 
 printf "\e[1;37m%s\e[0m\n" "`LANG=C date` [TEST]  -- Help Testing ----------------"
-HELP_S3_LS=$("${PROGRAM}" s3 lsr test -h 2>&1 | head -7 | tee /dev/stderr | grep "Lightweight shell script for Amazon Web Service Command Line Interface like AWS CLI.")
+HELP_S3_LS=$("${PROGRAM}" s3 list-recursive test -h 2>&1 | head -7 | tee /dev/stderr | grep "Lightweight shell script for Amazon Web Service Command Line Interface like AWS CLI.")
 HELP_S3_CAT=$("${PROGRAM}" s3 cat -h 2>&1 | head -7 | tee /dev/stderr | grep "Lightweight shell script for Amazon Web Service Command Line Interface like AWS CLI.")
 HELP_S3_CP=$("${PROGRAM}" s3 cp -h 2>&1 | head -7 | tee /dev/stderr | grep "Lightweight shell script for Amazon Web Service Command Line Interface like AWS CLI.")
 if [ "${HELP_S3_LS}" ] && [ "${HELP_S3_CAT}" ] && [ "${HELP_S3_CP}" ]; then
@@ -22,28 +22,28 @@ else
 fi
 
 printf "\e[1;37m%s\e[0m\n" "`LANG=C date` [TEST]  -- List objects (MaxKeys=${MaxKeys}) Testing ----------------"
-if ("${PROGRAM}" s3 lsr "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)"); then
+if ("${PROGRAM}" s3 list-recursive "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)"); then
   printf "\e[1;32m%s\e[0m\n" "`LANG=C date` [INFO]  -- List objects (MaxKeys=${MaxKeys}) Passed ----------------"
 else
   printf "\e[1;31m%s\e[0m\n" "`LANG=C date` [ERROR] == List objects (MaxKeys=${MaxKeys}) Failed ================"; exit 1
 fi
 
 printf "\e[1;37m%s\e[0m\n" "`LANG=C date` [TEST]  -- Get object Testing ----------------"
-if ("${PROGRAM}" s3 cat "$(MaxKeys=1000 "${PROGRAM}" s3 lsr "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | LANG=C sed s/[^[:graph:][:blank:]]*//g); then
+if ("${PROGRAM}" s3 cat "$(MaxKeys=1000 "${PROGRAM}" s3 list-recursive "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | grep -Ev '^$|\$' | head -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | LANG=C sed s/[^[:graph:][:blank:]]*//g); then
   echo; printf "\e[1;32m%s\e[0m\n" "`LANG=C date` [INFO]  -- Get objects Passed ----------------"
 else
   echo; printf "\e[1;31m%s\e[0m\n" "`LANG=C date` [ERROR] == Get objects Failed ================"; exit 1
 fi
 
 printf "\e[1;37m%s\e[0m\n" "$(LANG=C date) [TEST]  -- List objects (MaxKeys=${MaxKeys}) Testing ----------------"
-if ("${PROGRAM}" s3 lsr "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)"); then
+if ("${PROGRAM}" s3 list-recursive "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)"); then
   printf "\e[1;32m%s\e[0m\n" "`LANG=C date` [INFO]  -- List objects (MaxKeys=${MaxKeys}) Passed ----------------"
 else
   printf "\e[1;31m%s\e[0m\n" "`LANG=C date` [ERROR] == List objects (MaxKeys=${MaxKeys}) Failed ================"; exit 1
 fi
 
 printf "\e[1;37m%s\e[0m\n" "$(LANG=C date) [TEST]  -- Get object Testing ----------------"
-if ("${PROGRAM}" s3 cat "$(MaxKeys=1000 "${PROGRAM}" s3 lsr "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | LANG=C sed s/[^[:graph:][:blank:]]*//g); then
+if ("${PROGRAM}" s3 cat "$(MaxKeys=1000 "${PROGRAM}" s3 list-recursive "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)" | LANG=C sed s/[^[:graph:][:blank:]]*//g); then
   echo; printf "\n\e[1;32m%s\e[0m\n" "`LANG=C date` [INFO]  -- Get objects Passed ----------------"
 else
   echo; printf "\e[1;31m%s\e[0m\n" "`LANG=C date` [ERROR] == Get objects Failed ================"; exit 1
@@ -55,9 +55,9 @@ fi
 printf "\e[1;37m%s\e[0m\n" "$(LANG=C date) [TEST]  -- Copy (PUT/GET) object Testing ----------------"
 put=$(date +%s | tee ./b)
 # local to s3
-"${PROGRAM}" s3 cp ./b "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)tmp/date" >/dev/stderr
+"${PROGRAM}" s3 cp ./b "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)tmp/date" >/dev/stderr
 # s3 to local
-"${PROGRAM}" s3 cp "$("${PROGRAM}" s3 lsr | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)tmp/date" ./a >/dev/stderr
+"${PROGRAM}" s3 cp "$("${PROGRAM}" s3 list-recursive | grep -Ev '^$|\$' | tail -1 | sed s/[^[:blank:]]*[[:blank:]]*//)tmp/date" ./a >/dev/stderr
 get=$(cat ./a)
 current=$(date +%s)
 echo "put content  : ${put}"
